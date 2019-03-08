a04c0b142b133df3b22d50a141c4f16309878ea9
jonathan nshuti <nshutijonathan130@gmail.com>
2019-03-07 17:12:38 -0800

    [ch-set-server-#164463160] server configured successfully.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import user from './modals/user';
 
 //set up the express app
 
 const app=express();
+//parse incoming requests data 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 //get welcome message
 
 app.get('/',(req,res)=>{
@@ -18,6 +22,22 @@ app.get('/api/v1/users',(req,res)=>{
 		success:"true",
 		message:"successfully in",
 		epic:user
+	});
+
+});
+app.post('/api/v1/users',(req,res)=>{
+	const add={
+		id:user.length+1,
+		email:req.body.email,
+		firstname:req.body.firstname,
+		lastname:req.body.lastname,
+		password:req.body.password
+	}
+	user.push(add);
+	res.status(200).send({
+		success:"true",
+		message:"successfully added",
+		add
 	});
 
 });
