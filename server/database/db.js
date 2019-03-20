0db969f1c955018790b2bf61b9fe7be50baf3736
jonathan nshuti <nshutijonathan130@gmail.com>
2019-03-19 22:26:47 -0700

    [ch-set-db-#164690689] database configured correctly.

--- server/database/db.js
+++ server/database/db.js
@@ -6,4 +6,36 @@ const pool = new Pool({
   password: 'password',
   port: 5432,
 })
+pool.on('connect',()=>{
+console.log('connected to the database');
+});
+//create tables
+export const createTables=()=>{
+	//users table
+	const usersTable= `CREATE TABLE IF NOT EXIST
+	users(
+	userID SERIAL PRIMARY KEY NOT NULL,
+	email VARCHAR(20) NOT NULL,
+	firstName VARCHAR(20) NOT NULL,
+	lastName VARCHAR(20) NOT NULL,
+	password VARCHAR(50) NOT NULL
+	)`;
+	pool.query(usersTable)
+	.then((res)=>{
+		console.log(res);
+		pool.end();
+
+	})
+	.catch((err)=>{
+		console.log(err);
+		pool.end();
+
+	})
+	pool.on('remove',()=>{
+		console.log('client removed');
+
+	});
+
+
+}
 export default pool ;
